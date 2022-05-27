c05d379aeddcbfc2fd9edf702f9826a917d5398d
AriadnePaola <1105apg@gmail.com>
2022-05-27 17:55:52 -0500

    control de estados

--- state-management-example/src/App.vue
+++ state-management-example/src/App.vue
@@ -1,18 +1,39 @@
 <template>
+  <!--Link informativo para las diferencias entre vue2 y vue 3-->
+  <!-- https://javascript.plainenglish.io/differences-between-vue-2-and-vue-3-ee627e2c83a8-->
   <div id="app">
-    
-    
+    <AppHeader/>
+    <div class="flex flex-col md:flex-row">
+      <AppProfileForm/> <!--<AppProfileForm @submit="update($event)"/>-->
+      <AppProfileDisplay/> <!--<AppProfileDisplay :formData="formData" /> formData es una prop dentro de Profile display, y un data aqui en APP.VUE-->
+    </div>
   </div>
 </template>
 
 <script>
+import AppHeader from './components/App-Header.vue';
+import AppProfileForm from './components/App-Profile-Form.vue';
+import AppProfileDisplay from './components/App-Profile-Display.vue'
 
 
 export default {
   name: 'App',
+  // data() {
+  //   return{
+  //     formData:{}
+  //   }
+  // },
   components: {
-    
+    AppHeader,
+    AppProfileForm,
+    AppProfileDisplay
   }
+  // methods: {
+  //   update(formData) {
+  //     this.formData = formData
+  //     console.log(`evento 'submit' arroja: ${this.formData.name}, ${this.formData.occupation}`);
+  //   }
+  // }
 }
 </script>
 
--- state-management-example/src/main.js
+++ state-management-example/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import '@/assets/css/tailwind.css'
+import store from './store'
 
 Vue.config.productionTip = false
 
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+ 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  store
+  }).$mount('#app')
